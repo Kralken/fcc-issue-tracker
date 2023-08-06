f17e7671981ed1f452b23913a28ab408c7aae731
Clark Reyes <clarkenreyes@gmail.com>
2023-08-06 21:46:27 +0800

    feat: added comments for functional testing

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,23 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-  
+suite("Functional Tests", function () {
+  // Create an issue with every field: POST request to /api/issues/{project}
+  // Create an issue with only required fields: POST request to /api/issues/{project}
+  // Create an issue with missing required fields: POST request to /api/issues/{project}
+  // View issues on a project: GET request to /api/issues/{project}
+  // View issues on a project with one filter: GET request to /api/issues/{project}
+  // View issues on a project with multiple filters: GET request to /api/issues/{project}
+  // Update one field on an issue: PUT request to /api/issues/{project}
+  // Update multiple fields on an issue: PUT request to /api/issues/{project}
+  // Update an issue with missing _id: PUT request to /api/issues/{project}
+  // Update an issue with no fields to update: PUT request to /api/issues/{project}
+  // Update an issue with an invalid _id: PUT request to /api/issues/{project}
+  // Delete an issue: DELETE request to /api/issues/{project}
+  // Delete an issue with an invalid _id: DELETE request to /api/issues/{project}
+  // Delete an issue with missing _id: DELETE request to /api/issues/{project}
 });
