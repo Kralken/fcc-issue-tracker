a2bd6896db134c0955441b87b6f7d4516a7bc53f
Clark Reyes <clarkenreyes@gmail.com>
2023-08-07 11:02:37 +0800

    feat: added tests for POST method

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -2,13 +2,121 @@ const chaiHttp = require("chai-http");
 const chai = require("chai");
 const assert = chai.assert;
 const server = require("../server");
+const { isValidObjectId } = require("mongoose");
 
 chai.use(chaiHttp);
 
 suite("Functional Tests", function () {
   // Create an issue with every field: POST request to /api/issues/{project}
+  test("Post request to /api/issues/{project}", function (done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .post("/api/issues/testapi")
+      .type("form")
+      .send({
+        issue_title: "Test issue title",
+        issue_text: "this is where the issue test will appear",
+        created_by: "test_created_user",
+        assigned_to: "test_assigned_user",
+        status_text: "this is the current status",
+      })
+      .end(function (err, res) {
+        assert.equal(res.status, 200, "status should be 200");
+        assert.equal(
+          res.body.issue_title,
+          "Test issue title",
+          "issue title should be correct"
+        );
+        assert.equal(
+          res.body.issue_text,
+          "this is where the issue test will appear",
+          "issue text should be correct"
+        );
+        assert.isTrue(res.body.open, "issue should be open by default");
+        assert.isTrue(
+          isValidObjectId(res.body._id),
+          "ObjectId should be generated and valid"
+        );
+        assert.isTrue(
+          new Date(res.body.created_on) <= new Date(),
+          "date should be valid"
+        );
+        assert.isTrue(
+          new Date(res.body.updated_on) <= new Date(),
+          "updated on date should be valid"
+        );
+        assert.equal(
+          res.body.assigned_to,
+          "test_assigned_user",
+          "assigned user is correct"
+        );
+        assert.equal(
+          res.body.status_text,
+          "this is the current status",
+          "status text is correct"
+        );
+        done();
+      });
+  });
   // Create an issue with only required fields: POST request to /api/issues/{project}
+  test("Post request to /api/issues/{project} with optional fields not filled in", function (done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .post("/api/issues/testapi")
+      .type("form")
+      .send({
+        issue_title: "Test issue title",
+        issue_text: "this is where the issue test will appear",
+        created_by: "test_created_user",
+      })
+      .end(function (err, res) {
+        assert.equal(res.status, 200, "status should be 200");
+        assert.equal(
+          res.body.issue_title,
+          "Test issue title",
+          "issue title should be correct"
+        );
+        assert.equal(
+          res.body.issue_text,
+          "this is where the issue test will appear",
+          "issue text should be correct"
+        );
+        assert.isTrue(res.body.open, "issue should be open by default");
+        assert.isTrue(
+          isValidObjectId(res.body._id),
+          "ObjectId should be generated and valid"
+        );
+        assert.isTrue(
+          new Date(res.body.created_on) <= new Date(),
+          "date should be valid"
+        );
+        assert.isTrue(
+          new Date(res.body.updated_on) <= new Date(),
+          "updated on date should be valid"
+        );
+        assert.isEmpty(res.body.assigned_to, "assigned user is correct");
+        assert.isEmpty(res.body.status_text, "status text is correct");
+        done();
+      });
+  });
   // Create an issue with missing required fields: POST request to /api/issues/{project}
+  test("POST request to /api/issues/{project} with missing required fields", function (done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .post("/api/issues/testapi")
+      .type("form")
+      .send({
+        assigned_to: "test_assigned_user",
+        status_text: "this is the current status",
+      })
+      .end(function (err, res) {
+        assert.equal(res.body.error, "required field(s) missing");
+        done();
+      });
+  });
   // View issues on a project: GET request to /api/issues/{project}
   // View issues on a project with one filter: GET request to /api/issues/{project}
   // View issues on a project with multiple filters: GET request to /api/issues/{project}
